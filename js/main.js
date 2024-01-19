5851398b3447a3a33ff0d6721fd336e91c99452c
Yuosef <www.yoyoadel@gmail.com>
2024-01-19 17:06:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let advice = document.getElementById("advice");
+let adviceBtn = document.getElementById("random_advice");
+let adviceId = document.getElementById("advice_id")
+
+displayAdvice(fetch("https://api.adviceslip.com/advice").then((resolve) => resolve.json()));
+
+adviceBtn.addEventListener("click", () => displayAdvice(fetch("https://api.adviceslip.com/advice").then((resolve) => resolve.json())));
+
+async function displayAdvice(promise) {
+    advice.innerHTML = "";
+    let loading = document.createElement("div");
+    loading.className = "loading";
+    for (let i = 0; i < 3; ++i) {
+        let span = document.createElement("span");
+        loading.appendChild(span);
+    }
+    advice.appendChild(loading);
+    try {
+        let adviceData = await promise;
+        console.log(adviceData.slip.advice);
+        advice.innerHTML = `"${adviceData.slip.advice}"`;
+        adviceId.innerHTML = `${adviceData.slip.id}`;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
